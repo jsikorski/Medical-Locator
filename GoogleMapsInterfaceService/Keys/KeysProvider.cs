d8bc736643f81083d3d7266466cc89cb50b7bb1d
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-03-19 22:43:32 +0100

    Resolved problems with xml files

---
+++ GoogleMapsInterfaceService/Keys/KeysProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace GoogleMapsInterfaceService.Key
+{
+    public static class KeysProvider
+    {
+        public static readonly string GooglePlacesApiKey;
+
+        private const string KeyFilePath = @"Key\googlePlacesApiKey.txt";
+
+        static KeysProvider()
+        {
+            GooglePlacesApiKey = File.ReadAllText(KeyFilePath);
+        }
+    }
+}
