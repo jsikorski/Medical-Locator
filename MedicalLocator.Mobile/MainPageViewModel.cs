9745fcdb09f6942500ea12fc42a5e29b0c74aabd
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-03-20 15:29:08 +0100

    Basic interface for mobile application

--- MedicalLocator.Mobile/MainPageViewModel.cs
+++ MedicalLocator.Mobile/MainPageViewModel.cs
@@ -1,26 +1,45 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Device.Location;
+using System.Windows;
 using Caliburn.Micro;
+using Microsoft.Phone.Controls.Maps;
 
 namespace MedicalLocator.Mobile
 {
     public class MainPageViewModel
     {
+        public IEnumerable<Pushpin> MapPins { get; private set; }
+
         private readonly INavigationService _navigationService;
 
-        //public MainPageViewModel(INavigationService navigationService)
-        //{
-        //    _navigationService = navigationService;
-        //}
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            //MapPins = new List<Pushpin>
+            //              {
+            //                  new Pushpin() {Location = new GeoCoordinate(50.0, 25.0)},
+            //                  new Pushpin() {Location = new GeoCoordinate(45.0, 25.0)}
+            //              };
+        }
+
+        public void FindMe()
+        {
+            MessageBox.Show("To do...");
+        }
+
+        public void Search()
+        {
+            MessageBox.Show("To do...");
+        }
 
         public void OpenSettings()
         {
-            
-            MessageBox.Show("Hello from settings!");
+            MessageBox.Show("To do...");
         }
 
         public void OpenAboutPage()
         {
-            MessageBox.Show("Hello from about page!");
+            MessageBox.Show("To do...");
         }
     }
 }
