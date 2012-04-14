a42c8a9d85dd7e4a562a38be4e709da8ef75b9e6
Marek Sklodowski <masklod@gmail.com>
2012-04-14 19:13:20 +0200

    Basic login screen interface

---
+++ MedicalLocator.Mobile/Converters/MyBooleanToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MedicalLocator.Mobile.Converters
+{
+    public class MyBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (bool)value;
+
+            if (parameter != null && bool.Parse((string)parameter))
+                val = !val;
+
+            return val ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = ((value is Visibility) && (((Visibility)value) == Visibility.Visible));
+            if (parameter != null)
+            {
+                if ((bool)parameter)
+                {
+                    val = !val;
+                }
+            }
+            return val;
+        }
+
+    }
+}
