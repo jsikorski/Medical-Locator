abe49e19e0eed1a974ffbb35cca18556c8b0bcfa
Marek Sklodowski <masklod@gmail.com>
2012-04-16 19:47:11 +0200

    Moved medicaltype and centertype enums to dll

---
+++ MedicalLocator.Mobile/Model/EnumsValuesProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MedicalLocator.Mobile.DatabaseConnectionReference;
+using MedicalLocator.Mobile.ServicesReferences;
+
+namespace MedicalLocator.Mobile.Model
+{
+    public class EnumsValuesProvider : IEnumsValuesProvider
+    {
+        public IEnumerable<MedicalType> GetAllMedicalTypes()
+        {
+            return new[] { MedicalType.Dentist, MedicalType.Doctor };
+        }
+
+        public IEnumerable<CenterType> GetAllCenterTypes()
+        {
+            return new[] { CenterType.MyLocation, CenterType.Address, CenterType.Coordinates };
+        }
+    }
+}
---
+++ MedicalLocator.Mobile/Model/IEnumsValuesProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MedicalLocator.Mobile.DatabaseConnectionReference;
+using MedicalLocator.Mobile.ServicesReferences;
+
+namespace MedicalLocator.Mobile.Model
+{
+    public interface IEnumsValuesProvider
+    {
+        IEnumerable<MedicalType> GetAllMedicalTypes();
+        IEnumerable<CenterType> GetAllCenterTypes();
+    }
+}
