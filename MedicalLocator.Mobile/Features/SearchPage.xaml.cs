6dd0d616d56dcff4227ba7bc04ad22fbff3f26c5
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-04-16 21:37:58 +0200

    Test

--- MedicalLocator.Mobile/Features/SearchPage.xaml.cs
+++ MedicalLocator.Mobile/Features/SearchPage.xaml.cs
@@ -13,19 +13,11 @@ using Microsoft.Phone.Controls;
 
 namespace MedicalLocator.Mobile.Features
 {
-    public partial class SearchPage : PhoneApplicationPage
-    {
-        public SearchPage()
-        {
-            InitializeComponent();
-        }
-
-        private void MainPage_BindingValidationError(object sender, ValidationErrorEventArgs e)
-        {
-	        var state = e.Action == ValidationErrorEventAction.Added ? "Invalid" : "Valid";
-
-	        VisualStateManager.GoToState((Control)e.OriginalSource, state, false);
-        }
-
-    }
+	public partial class SearchPage : PhoneApplicationPage
+	{
+		public SearchPage()
+		{
+			InitializeComponent();
+		}
+	}
 }
---
+++ MedicalLocator.Mobile/Validation/ValidableTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MedicalLocator.Mobile.Validation
+{
+    public class ValidableTextBox : TextBox
+    {
+        public ValidableTextBox()
+        {
+            var style = (Style)Application.Current.Resources["ValidableTextBoxStyle"];
+            Style = style;
+            
+            BindingValidationError += OnBindingValidationError;
+        }
+
+        private void OnBindingValidationError(object sender, ValidationErrorEventArgs validationErrorEventArgs)
+        {
+            var state = validationErrorEventArgs.Action == ValidationErrorEventAction.Added ? "Invalid" : "Valid";
+            VisualStateManager.GoToState(this, state, false);
+        }
+    }
+}
