f03a8b8c41fe24e8852dfd6f7e07d9333d42aff1
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-05-06 18:59:35 +0200

    Moving

--- MedicalLocator.WebFront/Scripts/MedicalLocator/googleMaps.js
+++ MedicalLocator.WebFront/Scripts/MedicalLocator/googleMaps.js
@@ -1,9 +1,89 @@
 googleMapsManager = new GoogleMapsManager();
 
+function MedicalMarkerOptions() {
+    this.location = null;
+    this.name = null;
+    this.vicinity = null;
+    this.type = null;
+}
+
+MedicalMarkerType = {
+    User: 0,
+    Doctor: 1,
+    Dentist: 2,
+    Health: 3,
+    Hospital: 4,
+    Pharmacy: 5,
+    Physotherapist: 6,
+
+    fromNumber: function (number) {
+        switch (number) {
+            case 0:
+                return this.Doctor;
+            case 1:
+                return this.Dentist;
+            case 2:
+                return this.Health;
+            case 3:
+                return this.Hospital;
+            case 4:
+                return this.Pharmacy;
+            case 5:
+                return this.Physotherapist;
+        }
+    }
+};
+
+function MarkerTypeToIconUrlConverter() {
+    this.convert = function (markerType) {
+        var urlBase = "/Content/Graphics/Icons/";
+        var iconFileName;
+
+        switch (markerType) {
+            case MedicalMarkerType.User:
+                iconFileName = "user.png";
+                break;
+            case MedicalMarkerType.Doctor:
+                iconFileName = "doctor.png";
+                break;
+            case MedicalMarkerType.Dentist:
+                iconFileName = "dentist.png";
+                break;
+            case MedicalMarkerType.Health:
+                iconFileName = "health.png";
+                break;
+            case MedicalMarkerType.Hospital:
+                iconFileName = "hospital.png";
+                break;
+            case MedicalMarkerType.Pharmacy:
+                iconFileName = "pharmacy.png";
+                break;
+            case MedicalMarkerType.Physotherapist:
+                iconFileName = "physotherapist.png";
+                break;
+            default:
+                throw "Invalid marker type";
+        }
+
+        return urlBase + iconFileName;
+    };
+}
+
 function GoogleMapsManager() {
     var googleMapsApi = window.google.maps;
     var map;
     var markers = [];
+    var medicalTypeToMarkerUrlConverter = new MarkerTypeToIconUrlConverter();
+
+    var addInfoWindowForMarker = function (marker, markerName, vicinity) {
+        var contentString = "<div class='info_window_content'><h2>" + markerName + "</h2><p>" + vicinity + "</p></div>";
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        googleMapsApi.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+    };
 
     this.initializeMap = function (googleMapDomElement) {
         var mapOptions = {
@@ -15,31 +95,23 @@ function GoogleMapsManager() {
         map = new googleMapsApi.Map(googleMapDomElement, mapOptions);
     };
 
-    this.addMarker = function (markerLocation, markerName) {
+    this.addMarker = function (medicalMarkerOptions) {
+        var markerLocation = medicalMarkerOptions.location;
+        var markerName = medicalMarkerOptions.name;
+        var vicinity = medicalMarkerOptions.vicinity;
+        var markerType = medicalMarkerOptions.type;
+
         var position = new googleMapsApi.LatLng(markerLocation.Lat, markerLocation.Lng);
+        var markerIconUrl = medicalTypeToMarkerUrlConverter.convert(markerType);
+
         var newMarker = new googleMapsApi.Marker({
             position: position,
             map: map,
-            title: markerName
+            title: markerName,
+            icon: markerIconUrl
         });
 
-        var contentString = "<div class='info_window_content'><h1>" + markerName + "</h1></div>";
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        google.maps.event.addListener(infowindow, 'domready', function () {
-            $('.info_window_content').parent().parent().css('margin-top', '15px');
-            var height = $('.info_window_content').parent().parent().height();
-            height = height - 15;
-            $('info_window_content').parent().parent().height(height);
-        });
-
-        googleMapsApi.event.addListener(newMarker, 'click', function () {
-            infowindow.open(map, newMarker);
-        });
-
+        addInfoWindowForMarker(newMarker, markerName, vicinity);
         markers.push(newMarker);
     };
 
@@ -59,3 +131,6 @@ function GoogleMapsManager() {
         map.fitBounds(markersBounds);
     };
 }
+
+
+
