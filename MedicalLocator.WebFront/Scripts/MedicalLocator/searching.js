007b5527109bf5ff3331bcc40a02943224c1ad83
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-05-06 18:59:35 +0200

    Moving

--- MedicalLocator.WebFront/Scripts/MedicalLocator/searching.js
+++ MedicalLocator.WebFront/Scripts/MedicalLocator/searching.js
@@ -2,30 +2,47 @@ function SearchingManager() {
     
     var geoLocationProvider = new GeoLocationProvider();
 
-    var onLocationFoundForFindNearby = function(location) {
+    var onLocationFoundForFindNearby = function (location) {
+        var userLongitude = location.coords.longitude;
+        var userLatitude = location.coords.latitude;
+        
         $.ajax({
             url: "Searching/FindNearby",
-            data: { longitude: location.coords.longitude, latitude: location.coords.latitude },
-            success: onSearchSuccess
+            data: { longitude: userLongitude, latitude: userLatitude },
+            success: function (response) { onSearchSuccess(response, userLongitude, userLatitude); }
         });
     };
 
-    var onSearchSuccess = function (response) {
+    var onSearchSuccess = function (response, userLongitude, userLatitude) {
         if (response == "Failure") {
             busyIndicator.endBusy();
             return;
         }
         googleMapsManager.clearMarkers();
+        placeUserMarkerOnGoogleMaps(userLongitude, userLatitude);
         var foundObjects = response.Results;
         placeMarkersOnGoogleMaps(foundObjects);
-        busyIndicator.endBusy();        
+
+        busyIndicator.endBusy();
     };
 
-    var placeMarkersOnGoogleMaps = function(foundObjects) {
+    var placeUserMarkerOnGoogleMaps = function (longitude, latitude) {
+        var markerOptions = new MedicalMarkerOptions();
+        markerOptions.name = "Me";
+        markerOptions.vicinity = "User location.";
+        markerOptions.location = { Lat: latitude, Lng: longitude };
+        markerOptions.type = MedicalMarkerType.User;
+        googleMapsManager.addMarker(markerOptions);
+    };
+
+    var placeMarkersOnGoogleMaps = function (foundObjects) {
         for (var i = 0; i < foundObjects.length; i++) {
-            var location = foundObjects[i].Location;
-            var name = foundObjects[i].Name;
-            googleMapsManager.addMarker(location, name);
+            var markerOptions = new MedicalMarkerOptions();
+            markerOptions.name = foundObjects[i].Name;
+            markerOptions.location = foundObjects[i].Location;
+            markerOptions.vicinity = foundObjects[i].Vicinity;
+            markerOptions.type = MedicalMarkerType.fromNumber(foundObjects[i].Type);
+            googleMapsManager.addMarker(markerOptions);
         }
         googleMapsManager.fitMapBounds();
     };
