ca77aba700636a99c8d4ed8d1fde230c453a30d5
Marek Sklodowski <masklod@gmail.com>
2012-05-14 04:42:25 +0200

    Validating settings

--- MedicalLocator.Mobile/Features/SearchPage.xaml.cs
+++ MedicalLocator.Mobile/Features/SearchPage.xaml.cs
@@ -18,6 +18,73 @@ namespace MedicalLocator.Mobile.Features
         public SearchPage()
         {
             InitializeComponent();
+            RangeBindableTextBoxTextChanged(RangeTextBox, null);
+            RangeBindableTextBoxTextChanged(AddressTextBox, null);
+            RangeBindableTextBoxTextChanged(LatitudeTextBox, null);
+            RangeBindableTextBoxTextChanged(LongtitudeTextBox, null);
+        }
+
+        private void RangeBindableTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = ((TextBox)sender);
+
+            try
+            {
+                int val = Convert.ToInt32(textBox.Text);
+                if (val < 1 || val > 50000)
+                    textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                else
+                    textBox.BorderBrush = new SolidColorBrush(SystemColors.ActiveBorderColor);
+            }
+            catch (Exception ex)
+            {
+                textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                textBox.Text = "";
+            }
+
+            BindableTextBoxTextChanged(sender, e);
+        }
+
+        static private readonly string[] IncorectCharsInAddress = { "=", "!", "@", "#", "$", "%", "^", "&", "*", "?", "|", "'", "\"", "\n", "\r", "\t" };
+        private void AddressBindableTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = ((TextBox)sender);
+
+            try
+            {
+                if (textBox.Text.Length < 2 || IncorectCharsInAddress.Any(s => textBox.Text.Contains(s)))
+                    textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                else
+                    textBox.BorderBrush = new SolidColorBrush(SystemColors.ActiveBorderColor);
+            }
+            catch (Exception ex)
+            {
+                textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                textBox.Text = "";
+            }
+
+            BindableTextBoxTextChanged(sender, e);
+        }
+
+        private void CoordinatesBindableTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = ((TextBox)sender);
+
+            try
+            {
+                double val = Convert.ToDouble(textBox.Text);
+                if (val < -180 || val > 180)
+                    textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                else
+                    textBox.BorderBrush = new SolidColorBrush(SystemColors.ActiveBorderColor);
+            }
+            catch (Exception ex)
+            {
+                textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                textBox.Text = "";
+            }
+
+            BindableTextBoxTextChanged(sender, e);
         }
 
         private void BindableTextBoxTextChanged(object sender, TextChangedEventArgs e)
--- MedicalLocator.Mobile/Features/SearchPageViewModel.cs
+++ MedicalLocator.Mobile/Features/SearchPageViewModel.cs
@@ -10,6 +10,7 @@ using MedicalLocator.Mobile.DatabaseConnectionReference;
 using MedicalLocator.Mobile.Infrastructure;
 using MedicalLocator.Mobile.Model;
 using IContainer = Autofac.IContainer;
+using MedicalLocator.Mobile.Utils;
 
 namespace MedicalLocator.Mobile.Features
 {
@@ -68,8 +69,33 @@ namespace MedicalLocator.Mobile.Features
             PossibleSearchingCenterTypes = _enumsValuesProvider.GetAllCenterTypes();
         }
 
+        static private readonly string[] IncorectCharsInAddress = { "=", "!", "@", "#", "$", "%", "^", "&", "*", "?", "|", "'", "\"", "\n", "\r", "\t" };
         public void Search()
         {
+            if (SearchingRange < 1 || SearchingRange > 50000)
+            {
+                MessageBoxService.ShowError("Invalid range.");
+                return;
+            }
+
+            if (SelectedCenterTypeIndex == (int)CenterType.Address && (SearchedAddress.Length < 2 || IncorectCharsInAddress.Any(s => SearchedAddress.Contains(s))))
+            {
+                MessageBoxService.ShowError("Invalid address.");
+                return;
+            }
+
+            if (SelectedCenterTypeIndex == (int)CenterType.Coordinates && (SearchedLatitude < -180 || SearchedLatitude > 180 || SearchedLongitude < -180 || SearchedLongitude > 180))
+            {
+                MessageBoxService.ShowError("Invalid coordinates.");
+                return;
+            }
+
+            if (!SearchedMedicalTypes.Any(s => s.IsSelected))
+            {
+                MessageBoxService.ShowError("You must select at least one medical type.");
+                return;
+            }
+
             var command = _container.Resolve<Search>();
             CommandInvoker.Invoke(command);
         }
