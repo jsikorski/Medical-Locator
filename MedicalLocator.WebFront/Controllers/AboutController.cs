cbde23e87a548475e2a96fce82ab10788817703c
Marek Sklodowski <masklod@gmail.com>
2012-06-02 22:41:44 +0200

    About window

---
+++ MedicalLocator.WebFront/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using MedicalLocator.WebFront.Infrastructure;
+using MedicalLocator.WebFront.Models;
+using MedicalLocator.WebFront.Models.CommandsData;
+using MedicalLocator.WebFront.Services;
+using MedicalLocator.WebFront.ViewModels;
+
+namespace MedicalLocator.WebFront.Controllers
+{
+    public class AboutController : CommandsController
+    {
+        public ActionResult ShowAboutDialog()
+        {
+            return PartialView("_AboutDialogPartial");
+        }
+    }
+}
--- MedicalLocator.WebFront/Scripts/MedicalLocator/menu.js
+++ MedicalLocator.WebFront/Scripts/MedicalLocator/menu.js
@@ -17,8 +17,18 @@
         });
     };
 
+    var onAboutButtonClick = function () {
+        $.ajax({
+            url: "About/ShowAboutDialog",
+            success: function (response) {
+                dialogsManager.addDialogDiv();
+                dialogsManager.placeContentIntoDialogDiv(response);
+                dialogsManager.initializeDialog("About");
+            }
+        });
+    };
+
     var onLoginButtonClick = function () {
-        
         $.ajax({
             url: "Logging/ShowLoginDialog",
             success: function (response) {
@@ -47,6 +57,10 @@
         searchButtonElement.click(onSearchButtonClick);
     };
 
+    this.initializeAboutButton = function (aboutButtonElement) {
+        aboutButtonElement.click(onAboutButtonClick);
+    };
+    
     this.initializeLoginButton = function (loginButtonElement) {
         loginButtonElement.click(onLoginButtonClick);
     };
